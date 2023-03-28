2e28e32e1d360db006adff9097176e1838b52737
aartiwadagaonkar <106589221+aartiwadagaonkar@users.noreply.github.com>
2023-03-28 15:56:26 +0530

    initial commit

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use("*", (req, res) => {
     })
 })
 app.use(errorHandler)
-const PORT = process.env.PORT || 5000
+const PORT = process.env.VERCEL_PORT || 5000
 
 mongoose.connection.once("open", () => {
     app.listen(PORT, console.log(`SEVER RUNNING http://localhost:${PORT} `))
