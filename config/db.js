2d06825d26c3d3efdf37b336b05caaa7f6c03e09
aartiwadagaonkar <106589221+aartiwadagaonkar@users.noreply.github.com>
2023-03-28 16:15:27 +0530

    initial commit

--- config/db.js
+++ config/db.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose")
 const connectDB = () => {
     try {
         mongoose.set('strictQuery', true)
-        mongoose.connect(process.env.VERCEL_MONGO_URL)
+        mongoose.connect(process.env.DBHOST, {
+            useNewUrlParser: true,
+        })
     } catch (error) {
         console.log("MONGO ERROR" + error)
     }
