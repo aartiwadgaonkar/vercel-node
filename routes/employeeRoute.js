3b9b303f9a9d04996254529e6bdbe58ec8b8957a
aartiwadagaonkar <106589221+aartiwadagaonkar@users.noreply.github.com>
2023-03-28 15:15:37 +0530

    initial commit

---
+++ routes/employeeRoute.js
@@ -0,0 +1,17 @@
+const { registerEmployee, getAllEmployees, getSingleEmployees, updateEmployees, deleteEmployees, destroyEmployees, getEmployeeProfile, adminGetAllProducts, adminGetAllUsers, adminUserStatus, adminStat } = require("../controllers/employeeController")
+const { adminProtected } = require("../middleware/auth")
+const router = require("express").Router()
+
+router
+    .get("/", getAllEmployees)
+    .get("/profile", getEmployeeProfile)
+    .get("/admin-products", adminGetAllProducts)
+    .get("/detail/:employeeId", getSingleEmployees)
+    .post("/register", registerEmployee)
+    .put("/update/:employeeId", updateEmployees)
+    .delete("/delete/:employeeId", deleteEmployees)
+    .get("/users", adminGetAllUsers)
+    .get("/stat", adminStat)
+    .put("/users/status/:userId", adminUserStatus)
+    .delete("/destroy", destroyEmployees)
+module.exports = router
